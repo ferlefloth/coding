b68f17a21b862fa36a9ba8761f5567cca7a978a0
Mello <mellolefloth@gmail.com>
2021-04-19 01:19:30 -0300

    El Async y Await explicados

--- index.js
+++ index.js
@@ -90,7 +90,7 @@ createMovies({ title: `Return of the Jedi`,
 
 
 //-------------------COMO FUNCIONAN LAS PROMISES--------------
-const movies = [
+/*const movies = [
   { title: `A New Hope`, body:`After Princess Leia, the leader of the Rebel Alliance, is held hostage by Darth Vader, Luke and Han Solo must free her and destroy the powerful weapon created by the Galactic Empire.`},
   { title: `The Empire Strikes Back`, body: `Darth Vader is adamant about turning Luke Skywalker to the dark side. Master Yoda trains Luke to become a Jedi Knight while his friends try to fend off the Imperial fleet.` }
 ]
@@ -121,4 +121,59 @@ return new Promise((resolve, reject) => { //tenemos el Resolve y el Reject. El r
 }
 
 createMovies({ title: `Return of the Jedi`, body:`Luke Skywalker attempts to bring his father back to the light side of the Force. At the same time, the rebels hatch a plan to destroy the second Death Star.`})
-.then(getMovies); // El .then lo que hace es que , luego de realizarse la promesa, se ejecute el getMovies()
+.then(getMovies); // El .then lo que hace es que , luego de realizarse la promesa, se ejecute el getMovies()*/
+
+////////// EXPLICACIÓN DE ASYNC Y AWAIT /////////////////
+      ///// PRIMERA PARTE //////////
+/*async function example() {
+
+  let promise = new Promise((resolve, reject) => {
+    setTimeout(() => resolve("done!"), 2000)
+  });
+
+  let result = await promise; // wait until the promise resolves (*)
+
+  console.log(result); // "done!"
+}
+
+example();*/
+
+    /// SEGUNDA PARTE ///
+
+    const movies = [
+      { title: `A New Hope`, body:`After Princess Leia, the leader of the Rebel Alliance, is held hostage by Darth Vader, Luke and Han Solo must free her and destroy the powerful weapon created by the Galactic Empire.`},
+      { title: `The Empire Strikes Back`, body: `Darth Vader is adamant about turning Luke Skywalker to the dark side. Master Yoda trains Luke to become a Jedi Knight while his friends try to fend off the Imperial fleet.` }
+  ]
+
+function getMovies(){
+  setTimeout(() => {
+      movies.forEach((movie, index) => {
+          console.log(movie.title)
+      })
+  }, 1000);
+}
+
+function createMovies(movie){
+  return new Promise((resolve, reject) => {
+      setTimeout(() => {
+          movies.push(movie);
+
+          const error = false;
+
+          if(!error){
+              resolve();
+          }
+          else{
+              reject('Error: Something went wrong!')
+          }
+      }, 2000);
+  })
+}
+
+async function init(){
+  await createMovies({ title: `Return of the Jedi`, body:`Luke Skywalker attempts to bring his father back to the light side of the Force. At the same time, the rebels hatch a plan to destroy the second Death Star.`});
+  
+  getMovies();// (*) Esta función está esperando que se termine de ejecutar el await de createMovies
+}
+
+init();
